83c0076648f16da74f838d49f0a09238f4abc134
shinozaki-0722 <tellyourworld0722@gmail.com>
2020-07-27 11:56:09 +0900

    transactions/buy
    購入確認ページのビューファイル

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  root to: 'items#index'
+  get "transactions/buy"
+  get "mypages/index"
+  get "mypages/logout"
+  get "mypages/payment"
+  # resources :mypages, only: [:create,:index]
 end
